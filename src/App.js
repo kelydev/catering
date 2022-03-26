b8232656e81effd64cfbc7bfbe862d7ee568acc4
kely123 <kely.piscis.97@gmail.com>
2022-03-26 18:20:01 -0500

    s.10: Incorporacion del componente Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Footer from "./components/Footer";
+
 function App() {
+  const credits = {
+    author: 'Developer-team',
+    currentYear: new Date().getFullYear()
+  };
   return (
-    <header>
-      Hello World
-    </header>
-   
+    <>
+      <header className='header'> Hello World</header>
+      <main className='slider'>
+        <div>
+          <p>Slider</p>
+        </div>
+      </main>
+      <Footer
+        credits={credits}
+      /> 
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+import '../styles/css/Footer.css';
+const Footer = ({ credits }) => {
+    const { author, currentYear } = credits;
+    return ( 
+        <footer className="footer animate__animated animate__fadeIn">
+            <div className="footer-container">
+                <h2 className="footer__credits">🦄Copyright &copy; {currentYear} {author}. Todos los derechos reservados.🦄</h2>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
