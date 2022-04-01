cd635d948b22399c6157363b15dd9a85742fd417
kely123 <kely.piscis.97@gmail.com>
2022-04-01 11:39:41 -0500

    Incorporacion de estilos a los cards

--- src/pages/Carta.js
+++ src/pages/Carta.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react';
-
+import "../styles/sass/_carta.scss";
 const Carta = () => {
-
     const [producto, setProducto] = useState([]);
-
     useEffect (()=>{
         fetch('https://mariaalmenara.herokuapp.com/api/products')
         .then(response=> response.json())
@@ -11,21 +9,21 @@ const Carta = () => {
     },[]);
 
     return (
-      <section className="api">
+      <section className="productos">
         <div className="container">
           <h2>Lista de Productos</h2>
-            <div className="container__card row text-center">
-              {producto.map(productos => (
-                  <div className="col-md-6 col-lg-3">
-                  <div className="card api__card">
-                      <img className="card__img" src={productos.urlImage} alt="mortarboard"/>
-                      <div className="card-body">
-                          <h4 className="card-title">{productos.name}</h4>
-                          <p className="card-text">{productos.description}</p>
-                      </div>
-                  </div>
+          <div className="container__card row">
+            {producto.map(productos => (
+              <div className="card-product col-md-4 col-lg-3">
+                <img src={productos.urlImage} alt="helado" class="card-product__img"/>
+                <div class="card-product__body">
+                  <h4 class="card-product__title"><b>{productos.name}</b></h4> 
+                  <span class="card-product__candidad">1{productos.rating} porciones aproximadamente</span>
+                  <p class="card-product__precio">S/{productos.price}.00</p>
+                  <button class="btn__product">comprar ahora</button>
+                </div>     
               </div>   
-              ))}
+            ))}
           </div>
         </div>
       </section>
