11cdc62acbf8b04f0db3539aeee7c2de4eeb86ac
JosueEnriquez <elcolornoloestodo@gmail.com>
2022-04-08 15:15:13 -0500

    Componente category enlazado con la api

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,46 +1,48 @@
-import { useEffect } from 'react'
-import "../styles/sass/_category.scss"
-import HorizontalScroll from "react-scroll-horizontal"
+import { useEffect, useState } from "react";
+import "../styles/sass/_category.scss";
+import HorizontalScroll from "react-scroll-horizontal";
 
 export default function Category() {
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    let dispositivo
+    fetch("https://mariaalmenara.herokuapp.com/api/categories")
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      });
+    let dispositivo;
     if (navigator.userAgent.toLowerCase().match(/mobile/)) {
-      dispositivo = true
-    }else if (navigator.userAgent.toLowerCase().match(/tablet/)) {
-      dispositivo = true
-    }else {
-      dispositivo = false
+      dispositivo = true;
+    } else if (navigator.userAgent.toLowerCase().match(/tablet/)) {
+      dispositivo = true;
+    } else {
+      dispositivo = false;
     }
 
     if (dispositivo) {
-      document.querySelector('.scroll-horizontal').style.overflow = "auto"
+      document.querySelector(".scroll-horizontal").style.overflow = "auto";
     }
-
   }, []);
 
   return (
     <div className="category">
-        <ul className="category__list">
-          <HorizontalScroll>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-            <li className='category__list-item'><a className='category__list-link' href="/"><img className='category__list-link-image' src="https://dxnn4n4cam0ol.cloudfront.net/media/catalog/category/helado_2.svg" alt="category" /><span className='category__list-link-text'>helado</span></a></li>
-          </HorizontalScroll>
-        </ul>
+      <ul className="category__list">
+        <HorizontalScroll>
+          {categories.map((category) => (
+            <li className="category__list-item" key={category._id}>
+              <a className="category__list-link" href="/">
+                <img
+                  className="category__list-link-image"
+                  src={category.urlImage}
+                  alt="category"
+                />
+                <span className="category__list-link-text">{category.name}</span>
+              </a>
+            </li>
+          ))}
+        </HorizontalScroll>
+      </ul>
     </div>
-  )
+  );
 }
