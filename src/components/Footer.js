8e7edf24129a84b1e084f8562f2f01f49f49be62
JosueEnriquez <elcolornoloestodo@gmail.com>
2022-04-08 21:28:18 -0500

    Corrección de sintaxis en el componente Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,147 +1,82 @@
-import "../styles/sass/_footer.scss"
+import "../styles/sass/_footer.scss";
 const Footer = () => {
-    return ( <
-        footer className = "footer" >
-        <
-        nav className = "footer-nav" >
-        <
-        div className = "footer-nav__home-container" >
-        <
-        a className = "footer-nav__home-link" >
-        <
-        div className = "footer-nav__home-link-icon" > < /div> <
-        /a> <
-        p className = "footer-nav__home-info" >
-        @2020 copyright mariaalmenara.pe < br / >
-        todos los derechos reservados <
-        /p> <
-        /div> <
-        div className = "footer-nav__link-list-container" >
-        <
-        h5 className = "footer-nav__link-list-title" >
-        menú <
-        /h5> <
-        ul className = "footer-nav__link-list" >
-        <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        inicio <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        nosotros <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        nuestra carta <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        contáctanos <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        términos y condiciones <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        libro de reclamaciones <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        preguntas frecuentes <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        faqs <
-        /a> <
-        /li> <
-        li className = "footer-nav__link-item" >
-        <
-        a className = "footer-nav__link" >
-        derechos arco <
-        /a> <
-        /li> <
-        /ul> <
-        /div> <
-        div className = "footer-nav__contact-container" >
-        <
-        h5 className = "footer-nav__contact-title" >
-        nuestras redes <
-        /h5> <
-        ul className = "footer-nav__contact-link-list" >
-        <
-        li className = "footer-nav__contact-link-item" >
-        <
-        a className = "footer-nav__contact-link" >
-        <
-        /a> <
-        /li> <
-        li className = "footer-nav__contact-link-item" >
-        <
-        a className = "footer-nav__contact-link" >
-        <
-        /a> <
-        /li> <
-        li className = "footer-nav__contact-link-item" >
-        <
-        a className = "footer-nav__contact-link" >
-        <
-        /a> <
-        /li> <
-        /ul> <
-        /div> <
-        div className = "footer-nav__store-container" >
-        <
-        h5 className = "footer-nav__store-title" >
-        pide también <
-        /h5> <
-        div className = "footer-nav__store-link-container" >
-        <
-        a className = "footer-nav__store-link" >
-        <
-        /a> <
-        a className = "footer-nav__store-link" >
-        <
-        /a> <
-        /div> <
-        /div> <
-        div className = "footer-nav__claims-container" >
-        <
-        h5 className = "footer-nav__claims-title" >
-        libro de reclamaciones <
-        /h5> <
-        a className = "footer-nav__claims-link" >
-
-        <
-        /a> <
-        /div> <
-        div className = "footer-nav__page-info-contianer" >
-        <
-        p className = "footer-nav__page-info" >
-        powered by mariaalmenara <
-        /p> <
-        p className = "footer-nav__page-info--modified-text" >
-        @mariaalmenara 2020 todos los derechos reservados <
-        /p> <
-        /div> <
-        /nav> <
-        /footer>
-    );
-}
+  return (
+    <footer className="footer">
+      <nav className="footer-nav">
+        <div className="footer-nav__home-container">
+          <a className="footer-nav__home-link">
+            <div className="footer-nav__home-link-icon"> </div>
+          </a>
+          <p className="footer-nav__home-info">
+            @2020 copyright mariaalmenara.pe <br />
+            todos los derechos reservados
+          </p>
+        </div>
+        <div className="footer-nav__link-list-container">
+          <h5 className="footer-nav__link-list-title">menú </h5>
+          <ul className="footer-nav__link-list">
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">inicio </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">nosotros </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">nuestra carta </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">contáctanos </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">términos y condiciones </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">libro de reclamaciones </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">preguntas frecuentes </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">faqs </a>
+            </li>
+            <li className="footer-nav__link-item">
+              <a className="footer-nav__link">derechos arco </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-nav__contact-container">
+          <h5 className="footer-nav__contact-title">nuestras redes </h5>
+          <ul className="footer-nav__contact-link-list">
+            <li className="footer-nav__contact-link-item">
+              <a className="footer-nav__contact-link"></a>
+            </li>
+            <li className="footer-nav__contact-link-item">
+              <a className="footer-nav__contact-link"></a>
+            </li>
+            <li className="footer-nav__contact-link-item">
+              <a className="footer-nav__contact-link"></a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-nav__store-container">
+          <h5 className="footer-nav__store-title">pide también </h5>
+          <div className="footer-nav__store-link-container">
+            <a className="footer-nav__store-link"></a>
+            <a className="footer-nav__store-link"></a>
+          </div>
+        </div>
+        <div className="footer-nav__claims-container">
+          <h5 className="footer-nav__claims-title">libro de reclamaciones </h5>
+          <a className="footer-nav__claims-link"></a>
+        </div>
+        <div className="footer-nav__page-info-contianer">
+          <p className="footer-nav__page-info">powered by mariaalmenara </p>
+          <p className="footer-nav__page-info--modified-text">
+            @mariaalmenara 2020 todos los derechos reservados
+          </p>
+        </div>
+      </nav>
+    </footer>
+  );
+};
 export default Footer;
