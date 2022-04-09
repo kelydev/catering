718573328c3bd604a0366b64fb9437bf08d085f6
kely123 <kely.piscis.97@gmail.com>
2022-04-09 04:38:46 -0500

    Refactor:Agregando modificaciones al carrusel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,31 +1,95 @@
+import React, {useRef, useEffect, useCallback} from 'react';
+import {ReactComponent as FlechaIzquierda} from '../assets/images/iconmonstr-angel-left-thin.svg';
+import {ReactComponent as FlechaDerecha} from '../assets/images/iconmonstr-angel-right-thin.svg';
 import "../styles/sass/_carousel.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
-const Carousel = () => {
-    return (
-        <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="https://capricathemes.com/opencart/OPC08/OPC080184/image/cache/catalog/main-banner-2-1903x750.jpg" class="d-block" alt="slider1"/>
-                </div>
-                <div class="carousel-item" data-bs-interval="0">
-                    <img src="https://capricathemes.com/opencart/OPC08/OPC080184/image/cache/catalog/main-banner-1-1903x750.jpg" class="d-block" alt="slider2"/>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                <div className="carousel__button">
-                    <FontAwesomeIcon icon={solid("chevron-left")} aria-hidden="true" className="control-next-icon"/>
-                    <span class="visually-hidden">Previous</span> 
-                </div>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                <div className="carousel__button">
-                    <FontAwesomeIcon icon={solid("chevron-right")} aria-hidden="true" className="control-next-icon"/>
-                    <span class="visually-hidden">Next</span>  
-                </div>
-            </button>
-        </div>
-    );
+export default function Carousel() {
+	const controles = true
+	const autoplay = false
+	const velocidad="500"
+	const intervalo="5000"
+	const slideshow = useRef(null);
+	const intervaloSlideshow = useRef(null);
+
+	const siguiente = useCallback(() => {
+		if(slideshow.current.children.length > 0){
+			console.log('Siguiente')
+			const primerElemento = slideshow.current.children[0];
+			slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+			const tamSlide = slideshow.current.children[0].offsetWidth;
+			slideshow.current.style.transform = `translateX(-${tamSlide}px)`;
+			const transicion = () => {
+				slideshow.current.style.transition = 'none';
+				slideshow.current.style.transform = `translateX(0)`;
+				slideshow.current.appendChild(primerElemento);
+				slideshow.current.removeEventListener('transitionend', transicion);
+			}
+			slideshow.current.addEventListener('transitionend', transicion);
+
+		}
+	}, [velocidad]);
+	
+	const anterior = () => {
+		console.log('Anterior');
+		if(slideshow.current.children.length > 0){
+			const index = slideshow.current.children.length - 1;
+			const ultimoElemento = slideshow.current.children[index];
+			slideshow.current.insertBefore(ultimoElemento, slideshow.current.firstChild);
+			slideshow.current.style.transition = 'none';
+			const tamañoSlide = slideshow.current.children[0].offsetWidth;
+			slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+			setTimeout(() => {
+				slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+				slideshow.current.style.transform = `translateX(0)`;
+			}, 30);
+		}
+	}
+
+	useEffect(() => {
+		if(autoplay){
+			intervaloSlideshow.current = setInterval(() => {
+				siguiente();
+			}, intervalo);
+			slideshow.current.addEventListener('mouseenter', () => {
+				clearInterval(intervaloSlideshow.current);
+			});
+			slideshow.current.addEventListener('mouseleave', () => {
+				intervaloSlideshow.current = setInterval(() => {
+					siguiente();
+				}, intervalo);
+			});
+		}
+	}, [autoplay, intervalo, siguiente]);
+
+	return (
+		<div className='ContenedorPrincipal'>
+			<div className='ContenedorSlideshow'  ref={slideshow}>
+				<div className='Slide'>
+					<a href="#">
+						<img src="https://capricathemes.com/opencart/OPC08/OPC080184/image/cache/catalog/main-banner-2-1903x750.jpg" alt=""/>
+					</a>
+				</div>
+				<div className='Slide'>
+					<a href="#">
+						<img src="https://capricathemes.com/opencart/OPC08/OPC080184/image/cache/catalog/main-banner-1-1903x750.jpg" alt=""/>
+					</a>
+				</div>
+				<div className='Slide'>
+					<a href="#">
+						<img src="https://capricathemes.com/opencart/OPC08/OPC080184/image/cache/catalog/main-banner-1-1903x750.jpg" alt=""/>
+					</a>
+				</div>
+			</div>
+				{controles && 
+				<div className='Controles'>
+					<button className='Boton left' onClick={anterior}>
+						<FlechaIzquierda />
+					</button>
+					<button className='Boton right' onClick={siguiente}>
+						<FlechaDerecha/>
+					</button>
+				</div>
+				}
+		</div>
+	);
 }
-export default Carousel;
